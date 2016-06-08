92d7168d571ffe9cb52025ae42c5f45f09e32be0
Alex Atmore <acatmore@gmail.com>
2016-06-07 17:48:00 -0700

    finished the ceo distance while loop

--- public_html/index.php
+++ public_html/index.php
@@ -10,14 +10,14 @@
 </head>
 <body>
 <?php
-	//database info, change for your setup
-	$dbhost = "127.0.0.1";
-	$dbuser = "myuser";
-	$dbpass = "xxxx";
-	$dbname = "GoSolid";
+//database info, change for your setup
+$dbhost = "127.0.0.1";
+$dbuser = "myuser";
+$dbpass = "xxxx";
+$dbname = "GoSolid";
 
-	// Create connection
-	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+// Create connection
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 ?>
 <form method="Get">
@@ -31,19 +31,19 @@
  </form>
 
 <?php
-	//pagination variables
-	$employees_per_page = 100;
-	$page =1;
-	$sql_employees = "SELECT * FROM `employees`"; 
+//pagination variables
+$employees_per_page = 100;
+$page               = 1;
+$sql_employees      = "SELECT * FROM `employees`";
 
-	if ($rs_result = mysqli_query($conn, $sql_employees)) {
-		//return number of employees
-		$total_employees = mysqli_num_rows($rs_result);  
+if ($rs_result = mysqli_query($conn, $sql_employees)) {
+    //return number of employees
+    $total_employees = mysqli_num_rows($rs_result);
+    
+}
 
-	}
-
-	 //calculate $total pages
- 	$total_pages = ceil($total_employees / $employees_per_page);
+//calculate $total pages
+$total_pages = ceil($total_employees / $employees_per_page);
 ?>
  <table>
 	<tr class= "template">
@@ -53,85 +53,87 @@
 		<th class="template">Distance from CEO</th>
 	<tr>
 <?php
-	//convert sql col to html col 
-	$sql = "SELECT e.id as id, boss.id as idOfBoss, e.name as name, 
+//convert sql col to html col 
+$sql = "SELECT e.id as id, boss.id as idOfBoss, e.name as name, 
 	boss.name as boss, e.bossId as bossId, boss.bossId as idOfBossId FROM employees as e JOIN 
 	employees as boss ON e.bossId = boss.id ";
 
-	//url of search submission turned into sql query
-	if (isset($_GET['search'])) {
-		$search = trim ($_GET['search']); 
-		 //add query string search for like words
-		 $sql .= "WHERE e.name LIKE '%" . $search . "%' ";
-		 $search_result = $conn->query($sql);
-	} 
+//url of search submission turned into sql query
+if (isset($_GET['search'])) {
+    $search = trim($_GET['search']);
+    //add query string search for like words
+    $sql .= "WHERE e.name LIKE '%" . $search . "%' ";
+    $search_result = $conn->query($sql);
+}
 
-	// if order is needed to be set
-	// order by id
-	// $sql .= "ORDER BY id";
+// if order is needed to be set
+// order by id
+// $sql .= "ORDER BY id";
 
-	//only pull the number of rows you've set
-	$sql .= "LIMIT $employees_per_page ";
+//only pull the number of rows you've set
+$sql .= "LIMIT $employees_per_page ";
 
-	//url of page submission turned into query
-	if (isset($_GET["page"]) && is_numeric($_GET["page"])) {
-		$page = trim ($_GET["page"]);
-		$offset = (($page - 1) * $employees_per_page);
-		$sql .= "OFFSET " .$offset. " ";
-	} else {
-		$page=1;
-	}
+//url of page submission turned into query
+if (isset($_GET["page"]) && is_numeric($_GET["page"])) {
+    $page   = trim($_GET["page"]);
+    $offset = (($page - 1) * $employees_per_page);
+    $sql .= "OFFSET " . $offset . " ";
+} else {
+    $page = 1;
+}
 
-	/* while ($i = 1; $i) */
+/* while ($i = 1; $i) */
 
-	$data = $conn->query($sql);
-		//if can't find query
-		if (!$data) {
-		    printf("Error: %s\n", mysqli_error($conn));
-		    exit();
-		}
-		while ($record = mysqli_fetch_array($data)) {
-			echo "<tr>";
-			// echo "<td>" . $record['id'] . "</td>"; // id column if you want
-			echo "<td>" . $record['name'] . "</td>";
-			echo "<td>" . $record['boss'] . "</td>";
+$data = $conn->query($sql);
+//if can't find query
+if (!$data) {
+    printf("Error: %s\n", mysqli_error($conn));
+    exit();
+}
+while ($record = mysqli_fetch_array($data)) {
+    echo "<tr>";
+    // echo "<td>" . $record['id'] . "</td>"; // id column if you want
+    echo "<td>" . $record['name'] . "</td>";
+    echo "<td>" . $record['boss'] . "</td>";
+    
+    $current_boss_id = $record['id'];
+    $distance_to_ceo = 0;
+    
+    while ($current_boss_id != "1") {
+        $current_employee = "SELECT bossId FROM employees WHERE id =" . $current_boss_id . " ";
+        $employee_query   = $conn->query($current_employee);
+        $employee_array   = mysqli_fetch_array($employee_query);
+        $current_boss_id  = $employee_array['bossId'];
+        $distance_to_ceo += 1;
+    }
+    echo "<td>" . $distance_to_ceo . "</td>";
+    echo "</tr>";
+}
 
-			$current_boss = $record['bossId'];
-			$distance_to_ceo = 0;
+$next = $page += 1;
+$prev = $page -= 2;
+// if ($total_pages >= 1 && $page <= $total_pages) {
+//previous page view controls
+if (isset($_GET["search"])) {
+    $prev_button = "<a href=\"?search=" . $search . "\"?page=" . $prev . "\">" . 'prev' . " </a>";
+} else if (0 < $page) {
+    //back one page view
+    $prev_button = "<a href=\"?page=" . $prev . "\">" . 'prev' . " </a>";
+    echo $prev_button;
+}
+//next page view controls
+if (isset($_GET["search"])) {
+    $next_button = "<a href=\"?search=" . $search . "\"?page=" . $next . "\">" . 'next' . " </a>";
+} else if ($total_pages - 1 > $page) {
+    //forward one page view
+    $next_button = "<a href=\"?page=" . $next . "\">" . 'next' . " </a>";
+    echo $next_button;
+}
 
-			while ($current_boss['id'] != "1" ) {
-				 $current_employee = "SELECT id FROM employees WHERE bossId =" . $current_boss['id'] . " ";
-				 $current_boss = $conn->query($current_employee);
-				$distance_to_ceo += 1;
-				echo "<td>" . $distance_to_ceo . "</td>";
-			}
-			echo "</tr>";
-		}
+// }
 
- 	$next = $page += 1;
- 	$prev = $page -= 2;
- 	// if ($total_pages >= 1 && $page <= $total_pages) {
- 			//previous page view controls
- 	 		if (isset($_GET["search"])) {
- 	 			$prev_button = "<a href=\"?search=" .$search."\"?page=" .$prev."\">".'prev'." </a>";
- 	 		} else if (0 < $page){
- 	 		//back one page view
- 	 		$prev_button = "<a href=\"?page=" .$prev."\">".'prev'." </a>";
- 	 		echo $prev_button;
- 	 		}
- 	 		//next page view controls
- 	 		if (isset($_GET["search"])) {
- 	 			$next_button = "<a href=\"?search=" .$search."\"?page=" .$next."\">".'next'." </a>";
- 	 		} else if ($total_pages - 1 > $page) {
- 	 		//forward one page view
- 	 		$next_button = "<a href=\"?page=" .$next."\">".'next'." </a>";
- 	 		echo $next_button;
- 	 		}
-
- 	// }
-
-	//close connection
-	$conn->close();
+//close connection
+$conn->close();
 ?>
 
 </body>
